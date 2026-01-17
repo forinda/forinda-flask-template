c55367ebc079b871cf7440927bece3413d6c3d5e
Felix Orinda <forinda82@gmail.com>
2026-01-17 19:05:50 +0300

    feat: Add FLASK_DEBUG environment variable to start script

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 # Set Flask environment variables
 export FLASK_APP=main.py
 export FLASK_ENV=development
+export FLASK_DEBUG=1
 
 # Start Flask application with pipenv
 pipenv run flask run --host=0.0.0.0 --port=8000
